d080cf2b453c1a9b11e7ff72f46bfbd4bc522ac1
Avery <avery@fonsview.com>
2019-08-26 19:48:10 +0800

    update ServiceFactory

--- app/src/main/java/com/avery/newsplus/api/ServiceFactory.kt
+++ app/src/main/java/com/avery/newsplus/api/ServiceFactory.kt
@@ -4,14 +4,16 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object ServiceFactory {
+
     private const val BASE_URL = "http://api.dagoogle.cn/"
-    private val retrofit = lazy {
+
+    private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl(BASE_URL)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
 
-    fun <T> getService(serviceClass: Class<T>): T = retrofit.value.create(serviceClass)
+    fun <T> getService(serviceClass: Class<T>): T = retrofit.create(serviceClass)
 
 }
