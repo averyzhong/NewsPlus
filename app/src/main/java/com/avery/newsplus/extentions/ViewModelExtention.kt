fe91062c4d5c18176c79b0a2e45fea4f652320a1
Avery <avery@fonsview.com>
2019-12-02 15:39:25 +0800

    add Exception handler

--- app/src/main/java/com/avery/newsplus/extentions/ViewModelExtention.kt
+++ app/src/main/java/com/avery/newsplus/extentions/ViewModelExtention.kt
@@ -6,8 +6,12 @@ import kotlinx.coroutines.*
 import kotlinx.coroutines.launch
 import kotlin.coroutines.CoroutineContext
 
+var exceptionHandler = CoroutineExceptionHandler {  coroutineContext, throwable ->
+    print("Throws an exception with message: ${throwable.message}")
+}
+
 fun ViewModel.launch(
     context: CoroutineContext = viewModelScope.coroutineContext,
     start: CoroutineStart = CoroutineStart.DEFAULT,
     block: suspend CoroutineScope.() -> Unit
-): Job = viewModelScope.launch(context, start, block)
+): Job = viewModelScope.launch(context + exceptionHandler , start, block)
--- app/src/main/java/com/avery/newsplus/list/datasource/NewsMetaListDataSource.kt
+++ app/src/main/java/com/avery/newsplus/list/datasource/NewsMetaListDataSource.kt
@@ -3,6 +3,7 @@ package com.avery.newsplus.list.datasource
 import androidx.paging.PageKeyedDataSource
 import com.avery.newsplus.api.model.NewsMetaItem
 import com.avery.newsplus.list.repository.NewsListRepository
+import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 
@@ -11,18 +12,24 @@ class NewsMetaListDataSource(
     private val coroutineScope: CoroutineScope,
     private val repository: NewsListRepository
 ) : PageKeyedDataSource<Int, NewsMetaItem>() {
+
+    var exceptionHandler = CoroutineExceptionHandler {  coroutineContext, throwable ->
+        print("Throws an exception with message: ${throwable.message}")
+    }
+
     override fun loadInitial(params: LoadInitialParams<Int>, callback: LoadInitialCallback<Int, NewsMetaItem>) {
-        coroutineScope.launch {
+        coroutineScope.launch(exceptionHandler) {
             val response = repository.getNewsList(categoryId,1, params.requestedLoadSize)
             val newsMetaItems = response.body()?.data?.list
             newsMetaItems?.let {
                 callback.onResult(newsMetaItems, null, 2)
             }
+
         }
     }
 
     override fun loadAfter(params: LoadParams<Int>, callback: LoadCallback<Int, NewsMetaItem>) {
-        coroutineScope.launch {
+        coroutineScope.launch(exceptionHandler) {
             val response = repository.getNewsList(categoryId, params.key, params.requestedLoadSize)
             val newsMetaItems = response.body()?.data?.list
             newsMetaItems?.let {
