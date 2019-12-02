1f85d2ea0e13c93652646e1f27fe7dd63ad107c7
AveryZhong <1003759875@qq.com>
2019-12-02 23:05:40 +0800

    Commit MainActivity

---
+++ app/src/main/java/com/avery/newsplus/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.avery.newsplus
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * 首页Activity
+ *
+ * @author Avery
+ */
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
